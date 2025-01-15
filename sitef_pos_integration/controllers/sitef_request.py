5f3dfa4534385fb3490cdebd3b0b19f82e18b229
orlandoabarrientos <orlandoabarrientos@gmail.com>
2025-01-15 14:30:12 -0400

    Primeros pasos para la factura digital TFHKA
    1. Creamos el modulo tfhka_billing
    2. Se realizó el modulo de configuración de account

--- sitef_pos_integration/controllers/sitef_request.py
+++ sitef_pos_integration/controllers/sitef_request.py
@@ -2,16 +2,18 @@ from odoo import http
 import requests
 import logging
 import hashlib
+import json
 from bs4 import BeautifulSoup
+
 
 _logger = logging.getLogger(__name__)
 
 class SitefController(http.Controller):
     @http.route('/sitef_pos_integration/get_token', type='json', methods=['POST'])
-    def get_token(self, url, username, password):
+    def get_token(self, username, password):
         if username != "" and password != "":
             _logger.warning("INSIDE GET TOKEN")
-            response = requests.post(url + "/sitef/apiToken", json={
+            response = requests.post("https://api.sitefdevenezuela.com/prod/s4/sitef/apiToken", json={
                 "username": username,
                 "password": password
             })
@@ -27,13 +29,13 @@ class SitefController(http.Controller):
             return {"error": "Campo username o password vacío."}
     
     @http.route('/sitef_pos_integration/cambio_sitef', type='json', methods=['POST'])
-    def cambio_sitef(self, url, username, token, idbranch, codestall, destinationid, destinationmobilenumber, destinationbank, issuingbank, amount):
+    def cambio_sitef(self, username, token, idbranch, codestall, destinationid, destinationmobilenumber, destinationbank, issuingbank, amount):
         _logger.warning("INSIDE VUELTO SITEF")
         headers = {
             "Authorization": f"Bearer {token}"
         }
         token_md5 = hashlib.md5(token.encode()).hexdigest()
-        response = requests.post(url + "/sitefAuth/setVueltoSitef", json={
+        response = requests.post("https://api.sitefdevenezuela.com/prod/s4/sitefAuth/setVueltoSitef", json={
             "username": username, 
             "token": token_md5, 
             "idbranch": idbranch,
@@ -73,13 +75,13 @@ class SitefController(http.Controller):
             return {"error": f"Error en la solicitud: {response.status_code}"}
     
     @http.route('/sitef_pos_integration/validarPago_sitef', type='json', methods=['POST'])
-    def validarPago_sitef(self, url, username, token, idbranch, codestall, amount, paymentreference, debitphone, origenbank, receivingbank, trxdate):
+    def validarPago_sitef(self, username, token, idbranch, codestall, amount, paymentreference, debitphone, origenbank, receivingbank, trxdate):
         _logger.warning("INSIDE VALIDAR PAGO SITEF")
         headers = {
             "Authorization": f"Bearer {token}"
         }
         token_md5 = hashlib.md5(token.encode()).hexdigest()
-        response = requests.post(url + "/sitefAuth/getBusquedaSitef", json={
+        response = requests.post("https://api.sitefdevenezuela.com/prod/s4/sitefAuth/getBusquedaSitef", json={
             "username": username,
             "token": token_md5,
             "idbranch": idbranch,
@@ -117,13 +119,13 @@ class SitefController(http.Controller):
             return {"error": f"Error en la solicitud: {response.status_code}"}
         
     @http.route('/sitef_pos_integration/validarTransferencia_sitef', type='json', methods=['POST'])
-    def validarTransferencia_sitef(self, url, username, token, idbranch, codestall, amount, paymenreference, origenbank, origendni, trxdate, receivingbank):
+    def validarTransferencia_sitef(self, username, token, idbranch, codestall, amount, paymenreference, origenbank, origendni, trxdate, receivingbank):
         _logger.warning("INSIDE VALIDAR TRANSFERENCIA SITEF")
         headers = {
             "Authorization": f"Bearer {token}"
         }
         token_md5 = hashlib.md5(token.encode()).hexdigest()
-        response = requests.post(url + "/sitefAuth/getTrfSitef", json={
+        response = requests.post("https://api.sitefdevenezuela.com/prod/s4/sitefAuth/getTrfSitef", json={
             "username": username,
             "token": token_md5,
             "idbranch": idbranch,
@@ -161,13 +163,13 @@ class SitefController(http.Controller):
             return {"error": f"Error en la solicitud: {response.status_code}"}
         
     @http.route('/sitef_pos_integration/validarZelle_sitef', type='json', methods=['POST'])
-    def validarZelle_sitef(self, url, username, token, idbranch, codestall, amount, trxdate):
+    def validarZelle_sitef(self, username, token, idbranch, codestall, amount, trxdate):
         _logger.warning("INSIDE VALIDAR TRANSFERENCIA SITEF")
         headers = {
             "Authorization": f"Bearer {token}"
         }
         token_md5 = hashlib.md5(token.encode()).hexdigest()
-        response = requests.post(url + "/sitefAuth/getZelleSitef", json={
+        response = requests.post("https://api.sitefdevenezuela.com/prod/s4/sitefAuth/getZelleSitef", json={
             "username": username,
             "token": token_md5,
             "idbranch": idbranch,
@@ -217,3 +219,4 @@ class SitefController(http.Controller):
                 return round(precio_dolar, 2)
         else:
             print(f"Error: {respuesta.status_code}")
+       
