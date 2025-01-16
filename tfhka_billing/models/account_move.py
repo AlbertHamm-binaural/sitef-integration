23e896c4f3e8fcc9bda94bc868cd644493c761b1
orlandoabarrientos <orlandoabarrientos@gmail.com>
2025-01-16 16:04:49 -0400

    Funcion Generar Token
    1. Ya se realizó la funcion generar token en el modelo account_move.py
    2. Se eliminó la carpeta controllers por el momento

--- tfhka_billing/models/account_move.py
+++ tfhka_billing/models/account_move.py
@@ -1,10 +1,54 @@
 from odoo import models, api
-
+from odoo.exceptions import UserError
+import logging
+import requests
+_logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
+
     _inherit = 'account.move'
 
-    @api.model
-    def button_enviar(self, **kwargs):
-        print("Hello")
-        pass
+    def GenerarToken(self):
+        for move in self:
+            if move.company_id.username_tfhka and move.company_id.password_tfhka and move.company_id.url_tfhka:
+                username = move.company_id.username_tfhka
+                password = move.company_id.password_tfhka
+                url = move.company_id.url_tfhka
+                
+                respuesta = requests.post(url + "/Autenticacion", json={
+                    "usuario": username,
+                    "clave": password
+                })
+                respuesta_json = respuesta.json()
+                _logger.warning(respuesta_json)
+                
+                if respuesta.status_code == 200:
+                    if "token" in respuesta_json:
+                        token = respuesta_json["token"]
+                        _logger.info(token)
+                        return token, url
+                    else:
+                        _logger.error("La respuesta no contiene el token.")
+                    if respuesta_json.get("codigo") == 403:
+                        mensaje = respuesta_json.get("mensaje")
+                        _logger.warning(mensaje)
+                        raise UserError(mensaje)
+                else:
+                    _logger.error(f"Error: {respuesta.status_code}")
+            else:
+                _logger.error("USERNAME o PASSWORD vacío.")
+        
+        _logger.info(f"Username: {username}, Password: {password}, URL: {url}")
+        _logger.info("Hello")
+    
+    def Emision(self):          
+        url,token = self.GenerarToken()
+        _logger.info(f"URL: {url} / TOKEN: {token}")
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+        respuesta = requests.post(url + "/Emision", json={
+            "token": token,
+            
+        })
+        
